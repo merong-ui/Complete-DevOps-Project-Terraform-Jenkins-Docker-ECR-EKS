8e9ddf6971db3b10c7b36c937516ad7b1220f1dc
Meron <girmameron32@gmail.com>
2026-06-12 19:52:02 -0400

    modify the .tf files

--- Project-1/Terraform1/backend.tf
+++ Project-1/Terraform1/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "company-terraform-state"
+    bucket         = "for-jenkins-s3-terraform-state"
     key            = "devops/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true
