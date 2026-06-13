2decafef9a8dc2c0e15045e079733cfa97fb8399
Meron <girmameron32@gmail.com>
2026-06-13 09:32:54 -0400

    modify the .tf files

--- Project-1/Terraform1/ec2.tf
+++ Project-1/Terraform1/ec2.tf
@@ -16,9 +16,13 @@ resource "aws_instance" "jenkins" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.medium"
 
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  associate_public_ip_address = true
+
   iam_instance_profile = aws_iam_instance_profile.jenkins_profile.name
 
-  # User data script for initializing the Jenkins server that will install Docker, Jenkins, Dependencies, Configuration.
   user_data = file("${path.module}/user_data.sh")
 
   tags = {
