b486fce334ba34270b2160f9a6389936d6ee6dce
Meron <girmameron32@gmail.com>
2026-06-12 20:11:11 -0400

    modify the .tf files

--- Project-1/Terraform1/s3-backend.tf
+++ Project-1/Terraform1/s3-backend.tf
@@ -1,11 +1,12 @@
-# s3-backend.tf (bootstrap file)
+# s3-backend.tf 
+# This file sets up the S3 bucket for Terraform state storage and enables versioning to protect against accidental deletions or overwrites of the state file.
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "for-jenkins-s3-terraform-state"
 
-  tags = {
-    Name = "terraform-state"
-  }
+    tags = {
+        Name = "terraform-state-bucket"
+    }
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
