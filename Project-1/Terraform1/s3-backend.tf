17c52e6d467ee6ee1e2ba6338b2f4c31de985524
Meron <girmameron32@gmail.com>
2026-06-16 08:30:07 -0400

    s3 bucket name has been changed

--- Project-1/Terraform1/s3-backend.tf
+++ Project-1/Terraform1/s3-backend.tf
@@ -1,7 +1,7 @@
 # s3-backend.tf 
 # This file sets up the S3 bucket for Terraform state storage and enables versioning to protect against accidental deletions or overwrites of the state file.
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "for-jenkins-s3-terraform-state"
+  bucket = "for-jenkins-s3-terraform-state2"
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
