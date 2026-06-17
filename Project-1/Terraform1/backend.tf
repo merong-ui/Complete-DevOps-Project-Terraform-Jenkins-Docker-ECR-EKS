225a741ad6d059ce0470ac0d2338b61717abbc26
merong-ui <59237504+merong-ui@users.noreply.github.com>
2026-06-17 18:29:11 -0400

    Update S3 bucket name for Terraform backend

--- Project-1/Terraform1/backend.tf
+++ Project-1/Terraform1/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "for-jenkins-s3-terraform-state2"
+    bucket         = "<terraform-state-bucket>"
     key            = "devops/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true
