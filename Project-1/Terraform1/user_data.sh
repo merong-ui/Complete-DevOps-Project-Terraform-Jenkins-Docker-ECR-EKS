baeaea1bcc7801ec3a6d95696e827efa36a9f576
Meron <girmameron32@gmail.com>
2026-06-16 22:41:16 -0400

    update the file

--- Project-1/Terraform1/user_data.sh
+++ Project-1/Terraform1/user_data.sh
@@ -1,37 +1,101 @@
 #!/bin/bash
 set -xe
 
-# Update system packages
+#################################################
+# Update OS Packages
+#################################################
+
 dnf update -y
 
-# Install Docker on the host EC2 instance
+#################################################
+# Install Docker
+#################################################
+
 dnf install -y docker
+
 systemctl enable docker
 systemctl start docker
 
-# Add the default user to the docker group
+#################################################
+# Allow EC2 User To Run Docker Commands
+#################################################
+
 usermod -aG docker ec2-user
 
-# Pull the stable Jenkins image ahead of time
-docker pull jenkins/jenkins:lts-jdk21
+#################################################
+# Install Git
+#################################################
 
-# Launch Jenkins with the Host Docker Daemon Socket mounted
+dnf install -y git
+
+#################################################
+# Install unzip
+#################################################
+
+dnf install -y unzip
+
+#################################################
+# Install AWS CLI v2
+#################################################
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" \
+-o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+./aws/install
+
+#################################################
+# Install kubectl
+#################################################
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x kubectl
+
+mv kubectl /usr/local/bin/
+
+#################################################
+# Verify Installations
+#################################################
+
+docker --version
+
+git --version
+
+aws --version
+
+kubectl version --client
+
+#################################################
+# Create Jenkins Persistent Volume
+#################################################
+
+docker volume create jenkins_home
+
+#################################################
+# Run Jenkins Container
+#################################################
+
 docker run -d \
   --name jenkins \
   -p 8080:8080 \
   -p 50000:50000 \
-  -u root \
   -v jenkins_home:/var/jenkins_home \
   -v /var/run/docker.sock:/var/run/docker.sock \
   --restart unless-stopped \
   jenkins/jenkins:lts-jdk21
 
-# Wait a few seconds for Jenkins to initialize baseline directory paths
-sleep 10
+#################################################
+# Wait For Jenkins Startup
+#################################################
 
-# Automatically inject the Docker CLI binary directly inside the running Jenkins container
-docker exec -u 0 jenkins bash -c "
-  apt-get update && apt-get install -y curl
-  curl -fsSL https://get.docker.com -o get-docker.sh
-  sh get-docker.sh
-"
+sleep 60
+
+#################################################
+# Show Initial Admin Password
+#################################################
+
+docker logs jenkins > /home/ec2-user/jenkins.log
+
+chown ec2-user:ec2-user /home/ec2-user/jenkins.log
