ac0279afdefe963dc3e1b9e77613a01f995d44fd
merong-ui <59237504+merong-ui@users.noreply.github.com>
2026-06-17 18:31:58 -0400

    Change principal ARN to <ADMIN_IAM_ARN> in eks-access.tf
    Updated principal ARN for EKS access entries to use a placeholder for admin IAM role.

--- Project-1/Terraform1/eks-access.tf
+++ Project-1/Terraform1/eks-access.tf
@@ -2,7 +2,7 @@ resource "aws_eks_access_entry" "jenkins" {
 
   cluster_name = module.eks.cluster_name
 
-  principal_arn = aws_iam_role.jenkins_role.arn
+  principal_arn = <ADMIN_IAM_ARN>
 
   type = "STANDARD"
 
@@ -13,7 +13,7 @@ resource "aws_eks_access_policy_association" "jenkins_admin" {
 
   cluster_name = module.eks.cluster_name
 
-  principal_arn = aws_iam_role.jenkins_role.arn
+  principal_arn = <ADMIN_IAM_ARN>
 
   policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
