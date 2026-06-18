81597191ab77c6ea9157fc899aba7f0fb8c4c6f2
merong-ui <59237504+merong-ui@users.noreply.github.com>
2026-06-18 08:11:38 -0400

    Enhance backend.tf with comments for clarity
    Added comments to clarify S3 backend configuration.

--- Project-Folder/Terraform1/backend.tf
+++ Project-Folder/Terraform1/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "<terraform-state-bucket>"
-    key            = "devops/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
+    bucket         = "<terraform-state-bucket>"      # The name of the pre-existing S3 bucket
+    key            = "devops/terraform.tfstate"      # The file path inside the bucket where the state file will live
+    region         = "us-east-1"                     # The AWS region where the bucket resides
+    use_lockfile   = true                            # Enables state locking to prevent concurrent modifications
   }
 }
